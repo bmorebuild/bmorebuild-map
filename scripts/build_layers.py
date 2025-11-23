947a831c0182a17a5a47cc9a9215f4f43219d47b
bmorebuild <bmorebuild@proton.me>
2025-11-22 23:00:08 -0500

    Major update to build_layers. Output is now UNDER_CONSTRUCTION.geojson and COMPLETED_2025.geojson. Data is split based on new project_status and completed_year columns. No change to method of creating project shapes.

--- scripts/build_layers.py
+++ scripts/build_layers.py
@@ -12,13 +12,15 @@ PARCEL_KEY_COL       = "PIN"                # unique ID in parcel data
 PARCEL_ADDRESS_COL   = "MAILTOADD"          # readable address
 PARCEL_SDAT_COL      = "SDATLINK"           # link to SDAT detail page
 
-OUT_PARCELS          = "data/projects_parcels.geojson"
-OUT_PROJECT_POLYS    = "data/projects_polys.geojson"
+OUT_UNDER_CONSTRUCTION          = "data/UNDER_CONSTRUCTION.geojson"
+OUT_COMPLETED_2025    = "data/COMPLETED_2025.geojson"
 SIMPLIFY_TOL         = 0.00005              # tweak if polygons feel heavy
 # -----------------------------
 
 
 def load_parcels():
+    
+    print("Load geometries")
     parcels = gpd.read_file(PARCELS_FILE)
 
     if parcels.crs is None or parcels.crs.to_epsg() != 4326:
@@ -26,6 +28,8 @@ def load_parcels():
    
     # Normalize parcel key to string
     parcels[PARCEL_KEY_COL] = parcels[PARCEL_KEY_COL].astype(str).str.strip()
+    print(f"    Loaded {len(parcels)} city parcels")
+
 
     #Load custom polygons (optional override/supplement)
     try:
@@ -33,9 +37,13 @@ def load_parcels():
         if custom.crs is None or custom.crs.to_epsg() != 4326:
             custom = custom.to_crs(4326)
         custom[PARCEL_KEY_COL] = custom[PARCEL_KEY_COL].astype(str).str.strip()
-
+        print(f"    Loaded {len(custom)} custom parcels")
+        
         parcels = pd.concat([parcels, custom], ignore_index=True)
-        print(f"Loaded {len(custom)} custom parcel geometries")
+        print(f"    Combined into {len(parcels)} total shapes")
+       
+
+
     except FileNotFoundError:
         print("No custom_parcels.geojson found; skipping custom geometries.")
 
@@ -45,23 +53,27 @@ def load_parcels():
 
 def load_projects_table():
     
-    # Expect columns:
-    # parcel_id, project_id, project_name
-    parcels_tbl = pd.read_csv(PROJECTS_PARCELS_CSV, dtype={"parcel_id": str, "project_id": str, "project_name": str})
+
+    print("Load CSVs")
+    #load parcel csv file and retain all columns except project_name
+    parcels_tbl = pd.read_csv(PROJECTS_PARCELS_CSV, dtype={"parcel_id": str, "project_id": str, "project_status": str, "completed_year":str})
     
-    
-    #Under construction: combine the two so that more project data can be displayed
+    #load project list csv file and retain all columns
     proj_list = pd.read_csv(PROJECT_LIST_CSV,dtype={"project_id": str, "project_name": str, "project_link": str},
     )
 
-    projects = parcels_tbl.merge(
+    print(f"    project_parcels.csv: {len(parcels_tbl)} parcels")
+    print(f"    project_list.csv: {len(proj_list)} projects")
+
+    #join parcel csv and projects csv with project_id
+    project_data = parcels_tbl.merge(
         proj_list,
         on="project_id",
         how="left",     # parcels can exist even if project_list row missing
         validate="m:1", # many parcels -> 1 project
     )
-
-    return projects
+    print(f"    Total {len(project_data)} parcels with matching project")
+    return project_data
 
 
 
@@ -69,7 +81,8 @@ def build_project_layer(merged: gpd.GeoDataFrame):
     
     """
     Builds:
-      - projects_sites.geojson   (one feature per project_id)
+      - UNDER_CONSTRUCTION.geojson   (one feature per project_id)
+      - COMPLETED_2025.geojson (one feature per project_id)
     """
        
     # Split into non-custom vs custom based on PIN suffix
@@ -86,8 +99,10 @@ def build_project_layer(merged: gpd.GeoDataFrame):
     #Add  additional fields here.
     agg = {
         "project_id": "first",
-        "project_name_y": "first",
+        "project_name": "first",
         "project_link": "first",
+        "project_status": "first",
+        "completed_year": "first"
     }
 
     dissolved = non_custom.dissolve(by="project_id", aggfunc=agg)
@@ -96,14 +111,17 @@ def build_project_layer(merged: gpd.GeoDataFrame):
     #Convex hull = clean single polygon around them
     dissolved["geometry"] = dissolved.geometry.convex_hull
 
-    project_props = [
-        "project_name_y",
-        "project_link"
+    keep = [
+        "project_id",
+        "project_name",
+        "project_link",
+        "project_status",
+        "completed_year"
     ]
 
-    #Create non-customer geodataframe
+    #Create non-custom geodataframe
     gdf_non_custom = gpd.GeoDataFrame(
-        dissolved[project_props + ["geometry"]].copy(),
+        dissolved[keep + ["geometry"]].copy(),
         crs="EPSG:4326",
     )
 
@@ -113,15 +131,29 @@ def build_project_layer(merged: gpd.GeoDataFrame):
 
     # filter custom parcels to project_props and geometry
     gdf_custom = gpd.GeoDataFrame(
-        custom[project_props + ["geometry"]].copy(),
+        custom[keep + ["geometry"]].copy(),
         crs="EPSG:4326",
     )
 
     #append gdf_non_custom and gdf_custom
     gdf_combined = pd.concat([gdf_non_custom, gdf_custom], ignore_index=True)
 
-    gdf_combined.to_file(OUT_PROJECT_POLYS, driver="GeoJSON")
-    print(f"Wrote {OUT_PROJECT_POLYS} with {len(gdf_combined)} project features")
+
+    #split into two publishable layers
+    under_construction = gdf_combined[gdf_combined["project_status"] == "Under Construction"].copy()
+    completed_2025 = gdf_combined[(gdf_combined["project_status"] == "Completed") & (gdf_combined["completed_year"].astype(str) == "2025")].copy()
+
+
+    print("Creating project geometry")
+    print(f"    Under Construction:     {len(under_construction)} projects")
+    print(f"    Completed 2025:         {len(completed_2025)} projects")
+
+
+    under_construction.to_file(OUT_UNDER_CONSTRUCTION, driver="GeoJSON")
+    completed_2025.to_file(OUT_COMPLETED_2025, driver="GeoJSON")
+
+    print(f"Wrote {OUT_UNDER_CONSTRUCTION}")
+    print(f"Wrote {OUT_COMPLETED_2025}")
 
 
 
@@ -130,17 +162,19 @@ def main():
 
     parcels  = load_parcels()
 
-    projects = load_projects_table()
-    print(f"Loaded {len(parcels)} city parcels, {len(projects)} project parcel rows")
-    
+    project_data = load_projects_table()
+
+      
     merged = parcels.merge(
-        projects,
+        project_data,
         left_on=PARCEL_KEY_COL,
         right_on="parcel_id",
         how="inner",
         suffixes=("", "_proj"),
     )
-    print(f"Matched {len(merged)} parcels with custom projects")
+
+    merged = merged.rename(columns={"project_name_y": "project_name"})
+    
     build_project_layer(merged)
     print("Complete")
 
